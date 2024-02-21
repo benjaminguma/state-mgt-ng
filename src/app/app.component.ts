9f57822d17a060b2559178fd870aa97904e88455
Benjamin Guma <benjaminguma.bg@gmail.com>
2024-02-21 20:13:12 +0100

    pushhy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,41 @@
 import { Component } from '@angular/core';
+import { Counter } from './counter/counter.component';
+import { SignalCounter } from './counter/signal-counter.component';
+import { AuthService } from './auth.service';
+
+import { NgrxStoreModule } from './ngrx/store.module';
+import { AuthPageUsingService } from './auth-page.component';
+import { HttpClientModule } from '@angular/common/http';
+import { DataFetchingComponent } from './data-fetching';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { ClichComponent } from './clich/clich.component';
+import { DataFetchingComponent2 } from './data-fetching-2';
+import { ObservableCounter } from './observable.counter';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [
+    Counter,
+    SignalCounter,
+    AuthPageUsingService,
+    NgrxStoreModule,
+    HttpClientModule,
+    DataFetchingComponent,
+    DataFetchingComponent2,
+    ClichComponent,
+    ObservableCounter,
+  ],
+  providers: [],
+  template: `
+    <counter />
+    <signal-counter />
+    <auth-page-using-service />
+    <!-- <auth-page-using-ngrx /> -->
+    <data-fetching />
+    <!-- <data-fetching-2 /> -->
+    <clich />
+    <observable-counter />
+  `,
 })
-export class AppComponent {
-  title = 'state-mgt';
-}
+export class AppComponent {}
